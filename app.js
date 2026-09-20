a2611f342e4f92dcc05383cc3949d98902d9311f
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-20 23:58:05 +0200

    feat: add session handling with Prisma session store

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import path from "node:path";
 import express from "express";
 import { indexRouter } from "./routes/indexRouter.js";
 import { signUpRouter } from "./routes/signUpRouter.js";
+import passport from "passport";
+import "./config/passport.js"
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { prisma } from "./db/prisma.js";
 
 const app = express();
 
@@ -11,6 +16,27 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(import.meta.dirname, "public")));
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+		store: new PrismaSessionStore(prisma, {
+			checkPeriod: 2 * 60 * 1000,
+			dbRecordIdIsSessionId: true,
+			dbRecordIdFunction: undefined,
+		}),
+	}),
+);
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 app.use("/", indexRouter);
 app.use("/sign-up", signUpRouter)
