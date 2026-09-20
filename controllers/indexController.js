6032a4fa00162d5923a3d6332126f06871cae81d
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-21 00:22:39 +0200

    feat: list folders on index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,13 @@
-export function getIndex(req, res) {
-	res.render("index");
+import { prisma } from "../db/prisma.js";
+
+export async function getIndex(req, res) {
+	if (!req.user) {
+		return res.render("index", { folders: [] });
+	}
+
+	const folders = await prisma.folder.findMany({
+		where: { userId: req.user.id },
+	});
+
+	res.render("index", { folders });
 }
